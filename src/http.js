770c5ea47933e7e0ea734664b2f3df17c63d27f8
Youssouf Diallo <yd1265@nyu.edu>
2020-04-20 19:43:22 -0400

    deploy

--- src/http.js
+++ src/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 export default axios.create({
-  baseURL: 'http://localhost:8081/api',
+  baseURL: 'https://koune.herokuapp.com/api/',
 
   headers: {
     'Content-type': 'application/json'
